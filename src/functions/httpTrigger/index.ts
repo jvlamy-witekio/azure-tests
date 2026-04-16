7d13819ccfc8af2c727d33a3f55670b8bdc81d18
Jean-Vincent Lamy <jvlamy@witekio.com>
2026-04-16 08:14:39 +0200

    Remove angular & api

--- src/functions/httpTrigger/index.ts
+++ src/functions/httpTrigger/index.ts
@@ -12,6 +12,27 @@ import { BlobServiceClient } from "@azure/storage-blob";
 //   eventHubName: "eh1",
 // });
 
+function getBlobServiceClient(): BlobServiceClient {
+  const storageAccountConnectionString =
+    process.env["STORAGE_ACCOUNT_CONNECTION_STRING"];
+  const storageAccountName = process.env["STORAGE_ACCOUNT_NAME"];
+
+  if (!storageAccountConnectionString && !storageAccountName) {
+    throw new Error("Missing configuration");
+  }
+
+  if (storageAccountConnectionString) {
+    return BlobServiceClient.fromConnectionString(
+      storageAccountConnectionString,
+    );
+  } else {
+    return new BlobServiceClient(
+      `https://${storageAccountName}.blob.core.windows.net`,
+      new DefaultAzureCredential(),
+    );
+  }
+}
+
 export async function httpTrigger(
   request: HttpRequest,
   context: InvocationContext,
@@ -19,28 +40,13 @@ export async function httpTrigger(
   try {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    const storageAccountName = process.env["STORAGE_ACCOUNT_NAME"];
-    const containerName = process.env["STORAGE_CONTAINER_NAME"];
-    // const managedIdentityClientId = process.env["UAMI_CLIENT_ID"];
-
-    if (!storageAccountName || !containerName) {
-      return {
-        status: 500,
-        body: "Missing configuration",
-      };
+    const blobServiceClient = getBlobServiceClient();
+    const containerClient = blobServiceClient.getContainerClient("assets");
+    const containerExists = await containerClient.exists();
+    if (!containerExists) {
+      await blobServiceClient.createContainer(containerClient.containerName);
     }
 
-    const credential = new DefaultAzureCredential();
-
-    const blobServiceClient = new BlobServiceClient(
-      `https://${storageAccountName}.blob.core.windows.net`,
-      credential,
-    );
-
-    const containerClient = blobServiceClient.getContainerClient(containerName);
-    // await containerClient.getProperties();
-
-    // Itérateur async → performant
     const blobs: string[] = [];
     for await (const blob of containerClient.listBlobsFlat()) {
       blobs.push(blob.name);
@@ -49,14 +55,11 @@ export async function httpTrigger(
     return {
       status: 200,
       jsonBody: {
-        container: containerName,
-        count: blobs.length,
+        container: containerClient.containerName,
         blobs,
-        status: "ok",
       },
     };
   } catch (error) {
-    context.log(`Error: ${error}`);
     return { status: 500, body: `${error}` };
   }
 }
